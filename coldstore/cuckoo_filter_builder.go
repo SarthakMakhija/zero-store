44e60de149c488c8fa7e1ac59a439a2650297ce3
Sarthak <sarthak.makhija@gmail.com>
2024-12-11 14:08:22 +0530

    Sarthak | Adds cuckoo filter

---
+++ coldstore/cuckoo_filter_builder.go
@@ -0,0 +1,39 @@
+package coldstore
+
+import (
+	"github.com/SarthakMakhija/zero-store/kv"
+	cuckoo "github.com/seiflotfy/cuckoofilter"
+)
+
+const (
+	cuckooFilterCapacity           = 10_00_000
+	cuckooFilterCapacityMultiplier = 10
+)
+
+// FilterBuilder represents a cuckoo filter builder.
+type FilterBuilder struct {
+	keys []kv.Key
+}
+
+// NewCuckooFilterBuilder creates a new instance of cuckoo filter builder.
+func NewCuckooFilterBuilder() *FilterBuilder {
+	return &FilterBuilder{}
+}
+
+// Add adds the given key to its collection.
+func (builder *FilterBuilder) Add(key kv.Key) {
+	builder.keys = append(builder.keys, key)
+}
+
+// Build builds a new cuckoo filter.
+func (builder *FilterBuilder) Build() *cuckoo.Filter {
+	capacity := cuckooFilterCapacity
+	if len(builder.keys) >= cuckooFilterCapacity {
+		capacity = len(builder.keys) * cuckooFilterCapacityMultiplier
+	}
+	filter := cuckoo.NewFilter(uint(capacity))
+	for _, key := range builder.keys {
+		filter.Insert(key.RawBytes())
+	}
+	return filter
+}
--- go.mod
+++ go.mod
@@ -2,10 +2,14 @@ module github.com/SarthakMakhija/zero-store
 
 go 1.23.3
 
-require github.com/stretchr/testify v1.10.0
+require (
+	github.com/seiflotfy/cuckoofilter v0.0.0-20240715131351-a2f2c23f1771
+	github.com/stretchr/testify v1.10.0
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgryski/go-metro v0.0.0-20200812162917-85c65e2d0165 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
