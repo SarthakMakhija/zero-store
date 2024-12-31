4510aa63a6253f8a6ce175abf70b88ed93ed726b
Sarthak <sarthak.makhija@gmail.com>
2024-12-31 15:24:50 +0530

    Sarthak | Refactors block/iterator_test

--- objectstore/block/iterator_test.go
+++ objectstore/block/iterator_test.go
@@ -8,11 +8,11 @@ import (
 
 func TestBlockSeekToTheMatchingKey(t *testing.T) {
 	blockBuilder := NewBlockBuilderWithDefaultBlockSize()
-	blockBuilder.Add(kv.NewStringKey("consensus"), kv.NewStringValue("raft"))
-	blockBuilder.Add(kv.NewStringKey("etcd"), kv.NewStringValue("kv"))
+	blockBuilder.Add(kv.NewStringKeyWithTimestamp("consensus", 5), kv.NewStringValue("raft"))
+	blockBuilder.Add(kv.NewStringKeyWithTimestamp("etcd", 10), kv.NewStringValue("kv"))
 
 	block := blockBuilder.Build()
-	iterator := block.SeekToKey(kv.NewStringKey("etcd"))
+	iterator := block.SeekToKey(kv.NewStringKeyWithTimestamp("etcd", 10))
 	defer iterator.Close()
 
 	assert.True(t, iterator.IsValid())
@@ -24,11 +24,11 @@ func TestBlockSeekToTheMatchingKey(t *testing.T) {
 
 func TestBlockSeekToTheMatchingKeyWithCustomBlockSize(t *testing.T) {
 	blockBuilder := NewBlockBuilder(8192)
-	blockBuilder.Add(kv.NewStringKey("consensus"), kv.NewStringValue("raft"))
-	blockBuilder.Add(kv.NewStringKey("etcd"), kv.NewStringValue("kv"))
+	blockBuilder.Add(kv.NewStringKeyWithTimestamp("consensus", 5), kv.NewStringValue("raft"))
+	blockBuilder.Add(kv.NewStringKeyWithTimestamp("etcd", 10), kv.NewStringValue("kv"))
 
 	block := blockBuilder.Build()
-	iterator := block.SeekToKey(kv.NewStringKey("etcd"))
+	iterator := block.SeekToKey(kv.NewStringKeyWithTimestamp("etcd", 10))
 	defer iterator.Close()
 
 	assert.True(t, iterator.IsValid())
@@ -40,11 +40,11 @@ func TestBlockSeekToTheMatchingKeyWithCustomBlockSize(t *testing.T) {
 
 func TestBlockSeekToTheMatchingKeyFollowedByNext(t *testing.T) {
 	blockBuilder := NewBlockBuilderWithDefaultBlockSize()
-	blockBuilder.Add(kv.NewStringKey("consensus"), kv.NewStringValue("raft"))
-	blockBuilder.Add(kv.NewStringKey("etcd"), kv.NewStringValue("kv"))
+	blockBuilder.Add(kv.NewStringKeyWithTimestamp("consensus", 5), kv.NewStringValue("raft"))
+	blockBuilder.Add(kv.NewStringKeyWithTimestamp("etcd", 10), kv.NewStringValue("kv"))
 
 	block := blockBuilder.Build()
-	iterator := block.SeekToKey(kv.NewStringKey("consensus"))
+	iterator := block.SeekToKey(kv.NewStringKeyWithTimestamp("consensus", 5))
 	defer iterator.Close()
 
 	assert.True(t, iterator.IsValid())
@@ -59,17 +59,33 @@ func TestBlockSeekToTheMatchingKeyFollowedByNext(t *testing.T) {
 	assert.False(t, iterator.IsValid())
 }
 
-func TestBlockSeekToTheKeyGreaterThanTheSpecifiedKey(t *testing.T) {
+func TestBlockSeekToTheKeyWithTimestampLesserThanTheProvided(t *testing.T) {
 	blockBuilder := NewBlockBuilderWithDefaultBlockSize()
-	blockBuilder.Add(kv.NewStringKey("consensus"), kv.NewStringValue("raft"))
-	blockBuilder.Add(kv.NewStringKey("etcd"), kv.NewStringValue("kv"))
+	blockBuilder.Add(kv.NewStringKeyWithTimestamp("consensus", 10), kv.NewStringValue("raft"))
+	blockBuilder.Add(kv.NewStringKeyWithTimestamp("etcd", 5), kv.NewStringValue("kv"))
 
 	block := blockBuilder.Build()
-	iterator := block.SeekToKey(kv.NewStringKey("distributed"))
+	iterator := block.SeekToKey(kv.NewStringKeyWithTimestamp("etcd", 6))
 	defer iterator.Close()
 
 	assert.True(t, iterator.IsValid())
-	assert.Equal(t, kv.NewStringKey("etcd"), iterator.Key())
+	assert.Equal(t, kv.NewStringValue("kv"), iterator.Value())
+
+	_ = iterator.Next()
+	assert.False(t, iterator.IsValid())
+}
+
+func TestBlockSeekToTheKeyGreaterThanTheSpecifiedKey(t *testing.T) {
+	blockBuilder := NewBlockBuilderWithDefaultBlockSize()
+	blockBuilder.Add(kv.NewStringKeyWithTimestamp("consensus", 5), kv.NewStringValue("raft"))
+	blockBuilder.Add(kv.NewStringKeyWithTimestamp("etcd", 10), kv.NewStringValue("kv"))
+
+	block := blockBuilder.Build()
+	iterator := block.SeekToKey(kv.NewStringKeyWithTimestamp("distributed", 11))
+	defer iterator.Close()
+
+	assert.True(t, iterator.IsValid())
+	assert.Equal(t, kv.NewStringKeyWithTimestamp("etcd", 10), iterator.Key())
 	assert.Equal(t, "kv", iterator.Value().String())
 
 	_ = iterator.Next()
@@ -78,22 +94,22 @@ func TestBlockSeekToTheKeyGreaterThanTheSpecifiedKey(t *testing.T) {
 
 func TestBlockSeekToTheKeyGreaterThanTheSpecifiedKeyFollowedByNext(t *testing.T) {
 	blockBuilder := NewBlockBuilderWithDefaultBlockSize()
-	blockBuilder.Add(kv.NewStringKey("consensus"), kv.NewStringValue("raft"))
-	blockBuilder.Add(kv.NewStringKey("etcd"), kv.NewStringValue("kv"))
-	blockBuilder.Add(kv.NewStringKey("foundationDb"), kv.NewStringValue("distributed-kv"))
+	blockBuilder.Add(kv.NewStringKeyWithTimestamp("consensus", 5), kv.NewStringValue("raft"))
+	blockBuilder.Add(kv.NewStringKeyWithTimestamp("etcd", 6), kv.NewStringValue("kv"))
+	blockBuilder.Add(kv.NewStringKeyWithTimestamp("foundationDb", 7), kv.NewStringValue("distributed-kv"))
 
 	block := blockBuilder.Build()
-	iterator := block.SeekToKey(kv.NewStringKey("distributed"))
+	iterator := block.SeekToKey(kv.NewStringKeyWithTimestamp("distributed", 8))
 	defer iterator.Close()
 
 	assert.True(t, iterator.IsValid())
-	assert.Equal(t, kv.NewStringKey("etcd"), iterator.Key())
+	assert.Equal(t, kv.NewStringKeyWithTimestamp("etcd", 6), iterator.Key())
 	assert.Equal(t, "kv", iterator.Value().String())
 
 	_ = iterator.Next()
 
 	assert.True(t, iterator.IsValid())
-	assert.Equal(t, kv.NewStringKey("foundationDb"), iterator.Key())
+	assert.Equal(t, kv.NewStringKeyWithTimestamp("foundationDb", 7), iterator.Key())
 	assert.Equal(t, "distributed-kv", iterator.Value().String())
 
 	_ = iterator.Next()
@@ -102,14 +118,14 @@ func TestBlockSeekToTheKeyGreaterThanTheSpecifiedKeyFollowedByNext(t *testing.T)
 
 func TestBlockSeekToTheMatchingKeyWithADeletedValue(t *testing.T) {
 	blockBuilder := NewBlockBuilderWithDefaultBlockSize()
-	blockBuilder.Add(kv.NewStringKey("consensus"), kv.NewDeletedValue())
+	blockBuilder.Add(kv.NewStringKeyWithTimestamp("consensus", 5), kv.NewDeletedValue())
 
 	block := blockBuilder.Build()
-	iterator := block.SeekToKey(kv.NewStringKey("consensus"))
+	iterator := block.SeekToKey(kv.NewStringKeyWithTimestamp("consensus", 6))
 	defer iterator.Close()
 
 	assert.True(t, iterator.IsValid())
-	assert.Equal(t, kv.NewStringKey("consensus"), iterator.Key())
+	assert.Equal(t, kv.NewStringKeyWithTimestamp("consensus", 5), iterator.Key())
 	assert.Equal(t, "", iterator.Value().String())
 
 	_ = iterator.Next()
@@ -119,11 +135,11 @@ func TestBlockSeekToTheMatchingKeyWithADeletedValue(t *testing.T) {
 
 func TestBlockSeekToTheNonExistingKey(t *testing.T) {
 	blockBuilder := NewBlockBuilderWithDefaultBlockSize()
-	blockBuilder.Add(kv.NewStringKey("consensus"), kv.NewStringValue("raft"))
-	blockBuilder.Add(kv.NewStringKey("etcd"), kv.NewStringValue("kv"))
+	blockBuilder.Add(kv.NewStringKeyWithTimestamp("consensus", 5), kv.NewStringValue("raft"))
+	blockBuilder.Add(kv.NewStringKeyWithTimestamp("etcd", 6), kv.NewStringValue("kv"))
 
 	block := blockBuilder.Build()
-	iterator := block.SeekToKey(kv.NewStringKey("foundationDb"))
+	iterator := block.SeekToKey(kv.NewStringKeyWithTimestamp("foundationDb", 7))
 	defer iterator.Close()
 
 	assert.False(t, iterator.IsValid())
