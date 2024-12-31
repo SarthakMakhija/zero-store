2cf3e34fd211e21fedb8f5f9630742ce1173c261
Sarthak <sarthak.makhija@gmail.com>
2024-12-31 14:32:24 +0530

    Sarthak | Refactors bloom_filter_test

--- objectstore/filter/bloom_filter_test.go
+++ objectstore/filter/bloom_filter_test.go
@@ -8,41 +8,41 @@ import (
 
 func TestAddASingleKeyToBloomFilter(t *testing.T) {
 	builder := NewBloomFilterBuilder()
-	builder.Add(kv.NewStringKey("consensus"))
+	builder.Add(kv.NewStringKeyWithTimestamp("consensus", 5))
 
 	filter := builder.Build()
-	assert.True(t, filter.MayContain(kv.NewStringKey("consensus")))
+	assert.True(t, filter.MayContain(kv.NewStringKeyWithTimestamp("consensus", 5)))
 }
 
 func TestAddAFewKeysToBloomFilter(t *testing.T) {
 	builder := NewBloomFilterBuilder()
-	builder.Add(kv.NewStringKey("consensus"))
-	builder.Add(kv.NewStringKey("storage"))
-	builder.Add(kv.NewStringKey("zero disk"))
+	builder.Add(kv.NewStringKeyWithTimestamp("consensus", 2))
+	builder.Add(kv.NewStringKeyWithTimestamp("storage", 5))
+	builder.Add(kv.NewStringKeyWithTimestamp("zero disk", 6))
 
 	filter := builder.Build()
-	assert.True(t, filter.MayContain(kv.NewStringKey("consensus")))
-	assert.True(t, filter.MayContain(kv.NewStringKey("storage")))
-	assert.True(t, filter.MayContain(kv.NewStringKey("zero disk")))
+	assert.True(t, filter.MayContain(kv.NewStringKeyWithTimestamp("consensus", 5)))
+	assert.True(t, filter.MayContain(kv.NewStringKeyWithTimestamp("storage", 5)))
+	assert.True(t, filter.MayContain(kv.NewStringKeyWithTimestamp("zero disk", 5)))
 }
 
 func TestNonExistingKeysInBloomFilter(t *testing.T) {
 	builder := NewBloomFilterBuilder()
-	builder.Add(kv.NewStringKey("consensus"))
-	builder.Add(kv.NewStringKey("storage"))
-	builder.Add(kv.NewStringKey("zero disk"))
+	builder.Add(kv.NewStringKeyWithTimestamp("consensus", 2))
+	builder.Add(kv.NewStringKeyWithTimestamp("storage", 3))
+	builder.Add(kv.NewStringKeyWithTimestamp("zero disk", 4))
 
 	filter := builder.Build()
-	assert.False(t, filter.MayContain(kv.NewStringKey("disk")))
-	assert.False(t, filter.MayContain(kv.NewStringKey("cloud")))
-	assert.False(t, filter.MayContain(kv.NewStringKey("raw")))
+	assert.False(t, filter.MayContain(kv.NewStringKeyWithTimestamp("disk", 2)))
+	assert.False(t, filter.MayContain(kv.NewStringKeyWithTimestamp("cloud", 2)))
+	assert.False(t, filter.MayContain(kv.NewStringKeyWithTimestamp("raw", 2)))
 }
 
 func TestEncodeAndDecodeBloomFilter(t *testing.T) {
 	builder := NewBloomFilterBuilder()
-	builder.Add(kv.NewStringKey("consensus"))
-	builder.Add(kv.NewStringKey("storage"))
-	builder.Add(kv.NewStringKey("zero disk"))
+	builder.Add(kv.NewStringKeyWithTimestamp("consensus", 3))
+	builder.Add(kv.NewStringKeyWithTimestamp("storage", 5))
+	builder.Add(kv.NewStringKeyWithTimestamp("zero disk", 8))
 
 	buffer, err := builder.Build().Encode()
 	assert.NoError(t, err)
@@ -50,7 +50,7 @@ func TestEncodeAndDecodeBloomFilter(t *testing.T) {
 	filter, err := DecodeToBloomFilter(buffer)
 	assert.NoError(t, err)
 
-	assert.True(t, filter.MayContain(kv.NewStringKey("consensus")))
-	assert.True(t, filter.MayContain(kv.NewStringKey("storage")))
-	assert.True(t, filter.MayContain(kv.NewStringKey("zero disk")))
+	assert.True(t, filter.MayContain(kv.NewStringKeyWithTimestamp("consensus", 5)))
+	assert.True(t, filter.MayContain(kv.NewStringKeyWithTimestamp("storage", 5)))
+	assert.True(t, filter.MayContain(kv.NewStringKeyWithTimestamp("zero disk", 5)))
 }
