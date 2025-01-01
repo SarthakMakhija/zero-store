c08681bfecec91fcda9f7bd4cc5221aa6e5ed7f0
Sarthak <sarthak.makhija@gmail.com>
2025-01-01 19:23:04 +0530

    Sarthak | Removes the method NewStringKey (without timestamp)

--- kv/string_key.go
+++ kv/string_key.go
@@ -2,12 +2,6 @@
 
 package kv
 
-// NewStringKey creates a new instance of Key.
-// It is only used for tests.
-func NewStringKey(key string) Key {
-	return NewStringKeyWithTimestamp(key, 0)
-}
-
 // NewStringKeyWithTimestamp creates a new instance of Key.
 // It is only used for tests.
 func NewStringKeyWithTimestamp(key string, timestamp uint64) Key {
